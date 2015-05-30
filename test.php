7c4508348684c2c2a96b00a5771f8941b15e92dc
Joshua Zenn <wordman05@gmail.com>
2015-05-30 13:37:53 -0400

    Update test.php

--- test.php
+++ test.php
@@ -17,6 +17,20 @@
       }
     ?>
     <h1>Instructions</h1>
-    <p>You are about to take the test <bold><?php echo $testfullname; ?></bold>. </p>
+    <hr />
+    <p>You are about to take the test <bold><?php echo $testfullname; ?></bold>. Here are some things to keep in mind:</p>
+    <ul>
+      <li>DO NOT click the back button.</li>
+      <li>DO NOT click refresh.</li>
+      <li>DO NOT navigate away from the site until you see the results page.</li>
+      <li>DO NOT attempt to hack the system in any way. Doing so will blacklist your IP address.</li>
+      <li>DO NOT start the test if your browser does not support JavaScript.</li>
+      <li>DO NOT attempt to take this test with Internet Explorer.</li>
+      <li>DO read the question completely before answering.</li>
+      <li>DO read all the answers before answering.</li>
+      <li>DO notify your instructor when finished.</li>
+      <li>DO print out the results page for your records.</li>
+      <li>DO take your time.</li>
+    </ul>
   </body>
 </html>
