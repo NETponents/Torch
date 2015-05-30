496a6db6f7380a53d15e7761572ec1ba4545bef6
Joshua Zenn <wordman05@gmail.com>
2015-05-30 13:32:33 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,22 @@
-
+<html>
+  <header>
+    
+  </header>
+  <body>
+    <?php
+    $testpath = $_GET['t'];
+    $testslist = simplexml_load_file('./secure/test/tests.xml');
+    $testfullname = '';
+    for($i = 0; $i <= count($tests->test); $i++)
+      {
+        if($testslist->test[$i]->path = $testpath)
+        {
+          $testfullname = $testslist->test[$i]->title;
+          break;
+        }
+      }
+    ?>
+    <h1>Instructions</h1>
+    <p>You are about to take the test <bold><?php echo $testfullname; ?></bold>. </p>
+  </body>
+</html>
