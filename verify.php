5b259ed812cfd29fbb9daa9c74661d8649a70762
Joshua Zenn <wordman05@gmail.com>
2015-05-30 14:14:44 -0400

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,41 @@
 <?php
+$test_path = $_GET['t'];
+$test_name = $_GET['n'];
+$test_numquest = $_GET['tq'];
+$test_curquest = $_GET['q'];
+$test_correct = $_GET['r'];
+$auth_usr = $_GET['u'];
+$auth_secret = $_GET['sec'];
 
+$testauthsecret = '';
+
+$testslist = simplexml_load_file('./secure/test/tests.xml');
+for($i = 0; $i <= count($testslist->test); $i++)
+{
+  if($testslist->test[$i]->path = $testpath)
+  {
+    $testauthsecret = $testslist->test[$i]->pass;
+    break;
+  }
+}
+if($testauthsecret = $auth_secret)
+{
+  echo '<p>Click begin to start the test</p>';
+  ?>
+  <form method="post" action="./verifytest.php">
+  <input type="hidden" name="t" value="<?php echo $test_path; ?>" />
+  <input type="hidden" name="n" value="<?php echo $test_name; ?>" />
+  <input type="hidden" name="tq" value="<?php echo $test_numquest; ?>" />
+  <input type="hidden" name="q" value="0" />
+  <input type="hidden" name="r" value="n" />
+  <input type="hidden" name="u" value="<?php echo $auth_usr; ?>" />
+  <input type="submit" name="submit" value="Begin test" />
+  </form>
+<?php
+}
+else
+{
+  echo '<h3>Error 0x01. Please go back and try again.</h3>';
+  echo '<p>E(bad_auth_key)</p>';
+}
 ?>
