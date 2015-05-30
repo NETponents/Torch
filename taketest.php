ec20f7453159fd9762028fc0fa1e663f70d5bbc0
Joshua Zenn <wordman05@gmail.com>
2015-05-30 14:49:54 -0400

    Update taketest.php

--- taketest.php
+++ taketest.php
@@ -10,8 +10,6 @@ $debug = true;
 
 $testdata = simplexml_load_file('./secure/test/' . $test_path . '/test.xml');
 
-?>
-<?php
 if($debug == true)
 {
   echo $test_path . '<br />';
@@ -23,3 +21,45 @@ if($debug == true)
 }
 ?>
 <h1><?php echo $test_name; ?></h1>
+<?php
+if($curquest > 0)
+{
+  $prevans = $_GET['a'];
+  if($testdata->q[$curquest - 1]->c == $prevans)
+  {
+    $test_correct = $test_correct . 'r';
+  }
+  else
+  {
+    $test_correct = $test_correct . 'w';
+  }
+}
+if(curquest > numquest)
+{
+    echo '<p>Click finish test to see results.</p>
+  <form method="post" action="./results.php">
+  <input type="hidden" name="t" value="' . $test_path . '" />
+  <input type="hidden" name="n" value="' . $test_name . '" />
+  <input type="hidden" name="tq" value="' . $test_numquest . '" />
+  <input type="hidden" name="q" value="' . $test_curquest . '" />
+  <input type="hidden" name="r" value="' . $test_correct . '" />
+  <input type="hidden" name="u" value="' . $auth_usr . '" />
+  <input type="submit" name="submit" value="Finish test" />
+  </form>';
+}
+else
+{
+  echo '<h3>' . $testdata->q[$curquest]->quest . '</h3>';
+  echo 'Answer: 
+  <form method="post" action="./taketest.php">
+  <input type="hidden" name="t" value="' . $test_path . '" />
+  <input type="hidden" name="n" value="' . $test_name . '" />
+  <input type="hidden" name="tq" value="' . $test_numquest . '" />
+  <input type="hidden" name="q" value="' . $test_curquest + 1 . '" />
+  <input type="hidden" name="r" value="' . $test_correct . '" />
+  <input type="hidden" name="u" value="' . $auth_usr . '" />
+  <input type="text" name="a" />
+  <input type="submit" name="submit" value="Next >>" />
+  </form>';
+}
+?>
